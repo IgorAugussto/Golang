1dad87011fcd58c71c07202384c3f39b29abadcc
Igor Augusto <igor-augustofc@hotmail.com>
2024-02-18 00:55:47 -0300

    Adding some functions to the CRUD

--- Conexao_DB/Bacno de dados/main.go
+++ Conexao_DB/Bacno de dados/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+
 	//Esse underline é blank identfy, que significa que ele vai ignorar esse import até o momento de ele ser necessário
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
@@ -27,15 +29,36 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+
 	product := NewProduct("NoteBook", 1899.90)
 	err = insertProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
+
+	product.Price = 100.00
+	err = updateProducts(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+	/*p, err := selectOneProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Product: %v, possui o preço de %.2f", p.Name, p.Price)*/
+
+	products, err := selectAllProducts(db)
+	if err != nil {
+		panic(err)
+	}
+	for _, p := range products {
+		fmt.Printf("Product: %v, possui o preço de %.2f\n", p.Name, p.Price)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
-	stmt, err := db.Prepare("inser into products(id, name, price) values(?, ?, ?)")
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
 	if err != nil {
 		return err
 	}
@@ -45,4 +68,53 @@ func insertProduct(db *sql.DB, product *Product) error {
 		return err
 	}
 	return nil
+}
+
+func updateProducts(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func selectOneProduct( /*ctx context.Context,*/ db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	/*Dessa forma o ctx entra nesse caso como um contador para por exemplo dar um tempo em uma pesquisa, então se demorar
+	muito para fazer a pesquisa ele encerraria a pesquisa*/
+	//err = stmt.QueryRowContext(ctx, id).Scan(&p.ID, &p.Name, &p.Price)
+	// QueryRow é busca de uma linha apenas
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	row, err := db.Query("select id, name, price from products")
+	if err != nil {
+		return nil, err
+	}
+	defer row.Close()
+	var products []Product
+	for row.Next() {
+		var p Product
+		err = row.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
 }
